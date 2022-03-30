06fb0bdd793b301251dad1b185cb0c4ecfd63379
le-yo <le-yo@leyo.com>
2022-03-30 14:50:56 +0300

    Added gateway config

--- config/sms.php
+++ config/sms.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    'gateway' => 'wasiliana', //change this to your preferred gateway
+
     //wasiliana
     'wasiliana_api_key'=>env('WASILIANA_API_KEY'),
     'wasiliana_sender_name'=>env('WASILIANA_SENDER_NAME'),
